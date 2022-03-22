6824df3e08ab527aeb53de176057a078cc6a8fec
tuanthinh16 <tuanthinhdz@gmail.com>
2022-03-22 13:36:55 +0700

    ok

--- src/templates/discusion.js
+++ src/templates/discusion.js
@@ -274,7 +274,7 @@ function Discusion() {
                         {/* Breadcrumbs */}
                         <div className="breadcrumbs d-flex flex-row align-items-center">
                             <ul>
-                                <li><a href="index.html">Home</a></li>
+                                <li><a href="/">Home</a></li>
                                 <li><a href="categories.html"><i className="fa fa-angle-right" aria-hidden="true" />Men's</a></li>
                                 <li className="active"><a href="#"><i className="fa fa-angle-right" aria-hidden="true" />Single Product</a></li>
                             </ul>
